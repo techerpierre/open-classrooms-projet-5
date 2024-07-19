9b06d26b362fb482b3c8e9c6e84bd1acaf5a40d4
Techer Pierre <techerpierre.pro@gmail.com>
2024-07-19 14:20:22 +0200

    add carroussel

--- assets/script.js
+++ assets/script.js
@@ -15,4 +15,62 @@ const slides = [
 		"image":"slide4.png",
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
-]
+];
+
+const banner = document.getElementById("banner");
+const bannerImg = banner.querySelector("img");
+const bannerTagline = banner.querySelector("p");
+const bannerDots = banner.querySelector("#bannerDots");
+const next = banner.querySelector("#next");
+const back = banner.querySelector("#back");
+
+function setSlider(index) {
+	const slide = slides[index];
+	bannerTagline.innerHTML = slide.tagLine;
+	bannerImg.src = `/assets/images/slideshow/${slide.image}`;
+	const dots = bannerDots.querySelectorAll("span");
+	dots[index].classList.add("dot_selected");
+	dots.forEach((_, i) => {
+		if (i != index)
+			dots[i].classList.remove("dot_selected");
+	})
+}
+
+for (let i = 0; i < slides.length; i++) {
+	const span = document.createElement("span");
+	span.classList.add("dot");
+	bannerDots.appendChild(span);
+}
+
+let current = 0;
+
+setSlider(current);
+
+document.addEventListener("DOMContentLoaded", () => {
+	setInterval(() => {
+		let index = 0;
+		if (current < slides.length - 1)
+			index = ++current;
+		else
+			index = current = 0;
+		setSlider(index);
+	}, 5000);
+});
+
+next.addEventListener("click", () => {
+	let index = 0;
+	if (current < slides.length - 1)
+		index = ++current;
+	else
+		index = current = 0;
+	setSlider(index);
+});
+
+back.addEventListener("click", () => {
+	let index = 0;
+	if (current > 0)
+		index = --current;
+	else
+		index = current = slides.length-1;
+	setSlider(index);
+});
